81856df6c4d97082b37dd936d448d85b4b5e2a6a
ashutoshsawarnya <atiwari54321@gmail.com>
2023-03-03 12:56:36 +0530

    Second Commit

--- screens/Quiz.js
+++ screens/Quiz.js
@@ -47,6 +47,7 @@ const Quiz = ({ navigation }) => {
   };
 
   const handleSelectedOption = (_option) => {
+    console.log(_option);
     if (_option === qus[ques].correct_answer) {
       setScore(score + 10);
       setShowRightAnswer(true);
